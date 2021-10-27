b904b723d4204fd669e09b99a822f03e7189591f
ZTMatvey <73705821+ZTMatvey@users.noreply.github.com>
2021-10-27 18:35:33 +0700

    Добавлены сообщения об ошибках при входе и регистрации

--- SpeedTyping_Angular-ASP.NET/ClientApp/src/components/pages/services/user-login.service.ts
+++ SpeedTyping_Angular-ASP.NET/ClientApp/src/components/pages/services/user-login.service.ts
@@ -1,18 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Injectable()
 export class UserLoginService {
   private url: string = "/api/Account/Login";
-  userName: string = "";
-  password: string = "";
-  constructor(private http: HttpClient) { }
+  formModel = this.formBuilder.group({
+    UserName: ['', Validators.required],
+    Password: ['', [Validators.required]]
+  });
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
 
   login()
   {
     var body = {
-      UserName: this.userName,
-      Password: this.password,
+      UserName: this.formModel.value.UserName,
+      Password: this.formModel.value.Password,
     };
     return this.http.post(this.url, body);
   }
--- SpeedTyping_Angular-ASP.NET/ClientApp/src/components/pages/text-write/text-write.component.ts
+++ SpeedTyping_Angular-ASP.NET/ClientApp/src/components/pages/text-write/text-write.component.ts
@@ -60,8 +60,6 @@ export class TextWriteComponent implements AfterViewInit, OnInit {
     this.toHomePage();
   }
   toHomePage(){
-    console.log("toHomePage");
-    
     this.router.navigate([""]);
   }
   ngOnInit(){
